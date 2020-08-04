e455d5d90966c2fa1f2467f539504230721dce99
Oak atsume <56260438+user951@users.noreply.github.com>
2020-08-03 22:40:13 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,45 @@
 name="Black-ufo"
 username=$(whoami)
 LIP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+#check for packet manager
+which apt >/dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    apt="True"
+else
+    apt="false"
+fi
+######
+which pkg >/dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    apt="false"
+    pkg="true"
+else
+    apt="false"
+    apt="false"
+fi
+######
+which pacman >/dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    pacman="true"
+else
+    pacman="false"
+fi
+#####
+which yum >/dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    yum="true"
+else
+    yum=false
+fi
+#packet manager-end
 
 filename="Framework"
-
+bad="[×] "
+good="[√] "
 #color
 red=$'\e[1;31m'
 grn=$'\e[1;32m'
@@ -38,3 +74,15 @@ else
   printf $end
 fi
 
+if command -v nc >/dev/null;
+then
+  printf $grn
+  echo "$good netcat found!"
+  printf $end
+else
+  printf $red
+  echo "$bad netcat not found!"
+  #install netcat
+  
+  
+fi
