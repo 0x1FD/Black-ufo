53442b1b96fc8784334541bd1465844222f2443d
Oak atsume <56260438+user951@users.noreply.github.com>
2020-08-03 22:29:54 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,40 @@
+#!/bin/bash
+name="Black-ufo"
+username=$(whoami)
+LIP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+
+filename="Framework"
+
+#color
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+#color-end
+#check for file 
+if [ ! -e "$filename" ]; then
+  printf $red
+  echo "[×] $filename moved or missing!"
+  printf $end
+  exit 0
+else
+  printf $grn
+  echo "[√] $filename found!"
+  printf $end
+fi
+#check-end
+#start installer!
+if ping -c 1 google.com >/dev/null; 2>/dev/null
+then
+  printf $grn
+  echo "[√] connection to network"
+  printf $end
+else
+  printf $red
+  echo "[×] connection to network"
+  printf $end
+fi
 
